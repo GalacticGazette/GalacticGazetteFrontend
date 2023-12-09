b613fb4eca10e4b2b342d43e2d6d63bbbe96e678
andre-cat <andrecat-@outlook.com>
2023-12-09 00:36:38 -0500

    Data file added

---
+++ src/Data.tsx
@@ -0,0 +1,232 @@
+import { MagazineDict } from "./Types";
+
+export const MAGAZINES: MagazineDict = {
+    1: {
+        title: "Exploring Mars: The Perseverance Mission",
+        imagePath: 'images/space/mars-mission.png',
+        date: '2031/01',
+        category: 'EXPLORATION',
+        price: 9
+    },
+    2: {
+        title: "Black Holes: Portals to the Universe",
+        imagePath: 'images/space/black-hole.png',
+        date: '2031/02',
+        category: 'ASTROPHYSICS',
+        price: 7
+    },
+    3: {
+        title: "The Milky Way: Our Home in the Cosmos",
+        imagePath: 'images/space/milky-way.png',
+        date: '2031/03',
+        category: 'ASTRONOMY',
+        price: 8
+    },
+    4: {
+        title: "The Magical Aurora Borealis: Mysteries Revealed",
+        imagePath: 'images/space/aurora-borealis.png',
+        date: '2031/04',
+        category: 'PHENOMENA',
+        price: 11
+    },
+    5: {
+        title: "Exoplanets: In Search of Habitable Worlds",
+        imagePath: 'images/space/saturn-exoplanets.png',
+        date: '2031/05',
+        category: 'ASTROBIOLOGY',
+        price: 12
+    },
+    6: {
+        title: "The Fascinating Gravity: How it Governs the Universe",
+        imagePath: 'images/space/gravity-waves.png',
+        date: '2031/06',
+        category: 'PHYSICS',
+        price: 10
+    },
+    7: {
+        title: "Satellites: Our Eyes on Space",
+        imagePath: 'images/space/satellite-navigating.png',
+        date: '2031/07',
+        category: 'TECHNOLOGY',
+        price: 10
+    },
+    8: {
+        title: "The Explosion of Stars: Supernovae and their Cosmic Legacy",
+        imagePath: 'images/space/star-explosion.png',
+        date: '2031/08',
+        category: 'ASTROPHYSICS',
+        price: 13
+    },
+    9: {
+        title: "The Artemis Mission: Return to the Moon",
+        imagePath: 'images/space/moon-astronaut.png',
+        date: '2031/09',
+        category: 'EXPLORATION',
+        price: 24
+    },
+    10: {
+        
+        title: "The Mysterious Wormholes: Cosmic Bridges",
+        imagePath: 'images/space/wormhole-astronaut.png',
+        date: '2031/10',
+        category: 'COSMOLOGY',
+        price: 20
+    },
+    11: {
+        
+        title: "The Big Bang: Origin of the Universe",
+        imagePath: 'images/space/big-bang.png',
+        date: '2031/11',
+        category: 'COSMOLOGY',
+        price: 7
+    },
+    12: {
+        
+        title: "The Hubble Space Telescope: Eye on the Deep Universe",
+        imagePath: 'images/space/hubble-telescope.png',
+        date: '2031/12',
+        category: 'ASTRONOMY',
+        price: 15
+    },
+    13: {
+        
+        title: "Space Stations: Laboratories in Orbit",
+        date: '2032/01',
+        imagePath: 'images/space/space-stations.png',
+        category: 'TECHNOLOGY',
+        price: 16
+    },
+    14: {
+        
+        title: "Einstein's Theory of Relativity: A Cosmic Revolution",
+        date: '2032/02',
+        imagePath: 'images/space/relativity-representation.png',
+        category: 'PHYSICS',
+        price: 19
+    },
+    15: {
+        
+        title: "The Solar System: Our Cosmic Neighborhood",
+        date: '2032/03',
+        imagePath: 'images/space/solar-system.png',
+        category: 'ASTRONOMY',
+        price: 20
+    },
+    16: {
+        
+        title: "Interstellar Voyages: Exploring Beyond Our Solar System",
+        date: '2032/04',
+        imagePath: 'images/space/spaceship-navigating.png',
+        category: 'EXPLORATION',
+        price: 10
+    },
+    17: {
+        
+        title: "Devouring Galaxies: A Scientific Journey into the Giant Black Holes of the Universe",
+        imagePath: 'images/space/supermassive-blackhole.png',
+        date: '2032/05',
+        category: 'ASTROPHYSICS',
+        price: 14
+    },
+    18: {
+        
+        title: "The Asteroid Belt: Guardians and Dangers of Space",
+        imagePath: 'images/space/asteroid-belt.png',
+        date: '2032/06',
+        category: 'ASTRONOMY',
+        price: 3
+    },
+    19: {
+        
+        title: "The Exploration of the Cosmic Oceans: Moons of Neptune",
+        imagePath: 'images/space/neptune-moons.png',
+        date: '2032/07',
+        category: 'ASTROBIOLOGY',
+        price: 10
+    },
+    20: {
+        
+        title: "The Multiverse: Do Other Universes Exist?",
+        imagePath: 'images/space/multiverse-representation.png',
+        date: '2032/08',
+        category: 'COSMOLOGY',
+        price: 5
+    },
+    21: {
+        
+        title: "Pulsars: Cosmic Beacons in the Universe",
+        imagePath: 'images/space/pulsar-ray.png',
+        date: '2032/09',
+        category: 'ASTROPHYSICS',
+        price: 3
+    },
+    22: {
+        
+        title: "Nebulae: Stellar Nurseries in the Cosmos",
+        imagePath: 'images/space/pink-nebulae.png',
+        date: '2032/10',
+        category: 'ASTROPHYSICS',
+        price: 2
+    },
+    23: {
+        
+        title: "The Voyager Probe: Explorer to the Farthest Reaches of the Solar System",
+        imagePath: 'images/space/galactic-city.png',
+        date: '2032/11',
+        category: 'EXPLORATION',
+        price: 1
+    },
+    24: {
+        
+        title: "The Connection between Space and Time: What is the Spacetime Continuum?",
+        imagePath: 'images/space/planetary-landscape.png',
+        date: '2032/12',
+        category: 'PHYSICS',
+        price: 5
+    },
+    25: {
+        
+        title: "The Great Galactic Wall: Interconnected Worlds in the Universe",
+        imagePath: 'images/space/star-wall.png',
+        date: '2033/01',
+        category: 'ASTRONOMY',
+        price: 7
+    },
+    26: {
+        
+        title: "Farming in the Cosmos: Space Agriculture and Sustainable Future",
+        imagePath: 'images/space/spatial-agriculture.png',
+        date: '2033/02',
+        category: 'ASTROBIOLOGY',
+        price: 14
+    },
+    27: {
+        
+        title: "Interplanetary Conversations: Fascinating Dialogues with Our Robotic Assistants in Space",
+        imagePath: 'images/space/robot-thinking.png',
+        date: '2033/03',
+        category: 'TECHNOLOGY',
+        price: 21
+    },
+    28: {
+        title: "Transformed Horizons: Living in the World of Smart Cities",
+        imagePath: 'images/space/smart-city.png',
+        date: '2033/04',
+        category: 'TECHNOLOGY',
+        price: 17
+    },
+    29: {
+        title: "Expanded Consciousness: Exploring the Limits of Mind in the Futuristic Age",
+        imagePath: 'images/space/sky-wall.png',
+        date: '2033/05',
+        category: 'TECHNOLOGY',
+        price: 13
+    },
+    30: {
+        title: "Our Refuge in the Cosmos: The Earth as a Complex and Fascinating System.",
+        imagePath: 'images/space/planet-earth.png',
+        date: '2033/06',
+        category: 'EXPLORATION',
+        price: 13
+    },
+};
