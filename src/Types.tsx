e21948da23b7d5e54e859ebbda1d56b299e13155
andre-cat <andrecat-@outlook.com>
2023-12-09 00:38:59 -0500

    File for types added

---
+++ src/Types.tsx
@@ -0,0 +1,9 @@
+import { MagazineProps } from "./Props";
+
+export type ButtonStyle = 'solid' | 'outlined' | 'default';
+
+export type ButtonSize = 'small' | 'medium' | 'large' | 'default';
+
+export type ButtonType = 'button' | 'submit' | 'reset' | 'default';
+
+export type MagazineDict = { [key: string]: MagazineProps }
